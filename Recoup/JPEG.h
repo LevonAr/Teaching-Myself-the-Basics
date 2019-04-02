4a67f299f27bca5367199a5ab84bf5da2c47c273
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-02 00:55:07 -0700

    Update JPEG.h

--- Recoup/JPEG.h
+++ Recoup/JPEG.h
@@ -1,12 +1,14 @@
 #include <stdint.h>
 
+#include <stdint.h>
+
 typedef uint8_t  BYTE;
 
 
-typedef struct
+typedef struct block
 {
 
     unsigned int first_bytes : 24;
     BYTE rest_of_JPEG[509];
 
-} block;
+} bloc;

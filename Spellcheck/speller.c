948a5a960fd9a74f22a224a5656e9ca0f1f86218
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-11 18:24:00 -0700

    Update speller.c

--- Spellcheck/speller.c
+++ Spellcheck/speller.c
@@ -245,3 +245,50 @@ unsigned int PJWHash(const char* str, unsigned int length)
 
    return hash;
 }
+
+
+//Sieve of eratosthenes
+
+// I have to modify this to return only the closest (or closest few) prime numbers to my hash_table_size instead of returning every prime less than it
+#define LIMIT 1500000 /*size of integers array*/
+#define PRIMES 100000 /*size of primes array*/
+
+int main()
+{
+    int i,j,numbers[LIMIT];
+    int primes[PRIMES];
+
+    /*fill the array with natural numbers*/
+    for (i= 0;i<LIMIT;i++)
+    {
+        numbers[i]=i+2;
+    }
+
+    /*sieve the non-primes*/
+    for (i=0;i<LIMIT;i++)
+    {
+        if (numbers[i]!=-1)
+        {
+            for (j=2*numbers[i]-2;j<LIMIT;j+=numbers[i])
+            {
+                numbers[j]=-1;
+            }
+        }
+    }
+
+    /*transfer the primes to their own array*/
+    j = 0;
+    for (i=0;i<LIMIT&&j<PRIMES;i++)
+    {
+       if (numbers[i]!=-1)
+            primes[j++] = numbers[i];
+    }
+
+    /*print*/
+    for (i=0;i<PRIMES;i++)
+    {
+        printf("%d\n",primes[i]);
+    }
+
+return 0;
+}

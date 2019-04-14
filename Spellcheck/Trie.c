5fc8a59ad8239f29fc962ebbe9603d74793cac84
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-14 16:28:25 -0700

    Update Trie.c

--- Spellcheck/Trie.c
+++ Spellcheck/Trie.c
@@ -101,16 +101,6 @@ bool check(const char *word)
             return check_bucket;
         }
 
-        /*if(strcmp(HT_Ptr->words[check_index]->word, lowercase_word)==0)
-        {
-            return true;
-        }
-
-        else
-        {
-            return false;
-        }*/
-
     }
 
     else

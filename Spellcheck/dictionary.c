2eb340364c1d9e0121142932b42def9079cac643
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-17 23:35:07 -0700

    Update dictionary.c

--- Spellcheck/dictionary.c
+++ Spellcheck/dictionary.c
@@ -371,3 +371,17 @@ static unsigned long
 
         return hash;
     }
+
+unsigned long
+    hash(unsigned char *str)
+    {
+	unsigned int hash = 0;
+	int c;
+
+	while (c = *str++)
+	    hash += c;
+
+	return hash;
+    }
+
+

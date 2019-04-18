bcc53ab368b87517c90d383d738d095e3e9320bb
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-17 23:46:52 -0700

    Update Trie.c

--- Spellcheck/Trie.c
+++ Spellcheck/Trie.c
@@ -441,3 +441,24 @@ unsigned int JSHash(const char* str, unsigned int length)
 
    return hash;
 }
+
+unsigned int ELFHash(const char* str, unsigned int length)
+{
+   unsigned int hash = 0;
+   unsigned int x    = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (hash << 4) + (*str);
+
+      if ((x = hash & 0xF0000000L) != 0)
+      {
+         hash ^= (x >> 24);
+      }
+
+      hash &= ~x;
+   }
+
+   return hash;
+}

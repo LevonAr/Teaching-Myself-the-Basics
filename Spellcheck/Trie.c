b50b820780e99095a3cd8485ff957e43091078a5
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-17 23:47:25 -0700

    Update Trie.c

--- Spellcheck/Trie.c
+++ Spellcheck/Trie.c
@@ -462,3 +462,17 @@ unsigned int ELFHash(const char* str, unsigned int length)
 
    return hash;
 }
+
+unsigned int BKDRHash(const char* str, unsigned int length)
+{
+   unsigned int seed = 131; /* 31 131 1313 13131 131313 etc.. */
+   unsigned int hash = 0;
+   unsigned int i    = 0;
+
+   for (i = 0; i < length; ++str, ++i)
+   {
+      hash = (hash * seed) + (*str);
+   }
+
+   return hash;
+}

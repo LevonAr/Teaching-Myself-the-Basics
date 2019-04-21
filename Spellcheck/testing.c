27ab0414fc58983bf5b87a6458e141a34c8ba766
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-20 17:45:56 -0700

    Create testing.c

---
+++ Spellcheck/testing.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+int main(){
+    struct node *p; //pointer to node structure
+    p = NULL;   //linked list is empty
+
+    append( &p,1);
+    return 0;
+}
+
+append( struct node **q, int num){
+    struct node *temp, *r;  //two pointers to struct node
+    temp = *q;
+
+    if(*q == NULL){
+        temp = malloc(sizeof(struct node));
+        temp -> data = num;
+        temp -> link = NULL;
+        *q = temp;
+    }
+    else{
+        temp = *q;
+        while( temp -> link != NULL)
+            temp = temp -> link;
+        r = malloc(sizeof(struct node));
+        r -> data = num;
+        r -> link = NULL;
+        temp -> link = r;
+    }
+}

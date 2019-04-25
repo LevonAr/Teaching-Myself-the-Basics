05f566e4d0a3f2819b1ecef7cb7ef22c8e9e8ab5
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-04-25 04:49:58 -0700

    Create experiments.c

---
+++ Spellcheck/experiments.c
@@ -0,0 +1,17 @@
+// At this point I have got a trie and a hashtable to succesfully work with the following time stats
+
+/*Trie: 
+TIME IN load:         0.35
+TIME IN check:        0.02
+TIME IN size:         0.00
+TIME IN unload:       0.00
+TIME IN TOTAL:        0.37
+*/
+
+//I have two goals now. 
+// Goal 1 - the more important goal: is to strenghten my understanding of how and why my programs work the way they do. 
+// In other words I will make small tweaks and see if the program still works, and if it doesn't go into why.
+// I especially feel like my hashtable program (although quicker) is very brittle, I will try to toughen it up. whatever that means
+
+// Goal 2: make me programs work faster.
+

501ab414518593fba0f699e48dfb32dce89b1f98
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-08 23:57:53 -0700

    Update dictionary.c
    New times

--- Spellcheck/dictionary.c
+++ Spellcheck/dictionary.c
@@ -3,11 +3,11 @@
 WORDS MISSPELLED:     955
 WORDS IN DICTIONARY:  143091
 WORDS IN TEXT:        17756
-TIME IN load:         0.10
+TIME IN load:         0.12
 TIME IN check:        0.02
 TIME IN size:         0.00
-TIME IN unload:       0.02
-TIME IN TOTAL:        0.13
+TIME IN unload:       0.04
+TIME IN TOTAL:        0.18
 
 */
 

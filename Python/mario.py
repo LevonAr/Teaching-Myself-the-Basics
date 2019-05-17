d3b0f351cf7bea60a5f96b12f92ddd93fc5fb005
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-16 23:34:07 -0700

    Update mario.py

--- Python/mario.py
+++ Python/mario.py
@@ -1,8 +1,13 @@
 height = int(input("Height:"))
 while height < 0 or height > 23:
-    int(input("Height:"))
-for i in range((height-1),0,-1):
-    j = i-1
-    k = height+1-j
-    print(' '*j,'#'*k)
-print('#'*(height+1))
+    height = int(input("Height:"))
+if isinstance(height, int):
+    for i in range((height-1),0,-1):
+        j = i-1
+        k = height-j
+        print(' '*j,'#'*k)
+    print('#'*(height+1))
+else:
+    exit()
+
+

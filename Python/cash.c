fae60784a0285c948a8a4865ba8dc4f7cc6bb35e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-18 23:47:58 -0700

    Update cash.c

--- Python/cash.c
+++ Python/cash.c
@@ -1,7 +1,19 @@
 change = input("change owed:")
 
+slice_change = change//1
+
 whole_num_change = change*100
 
 quarters = whole_num_change//25
 
 quarter_rem = whole_num_change%25
+
+dimes = quarter_rem//10
+
+dime_rem = quarter_rem%10
+
+nickels = dime_rem//5
+
+nickel_rem = dime_rem%5
+
+pennies = nickel_rem

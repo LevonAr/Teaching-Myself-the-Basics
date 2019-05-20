03c691abc6d32022ba2b7e7a93f4d60971b678e3
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-20 14:00:40 -0700

    Update credit.py

--- Python/credit.py
+++ Python/credit.py
@@ -11,8 +11,32 @@ for i in range(num_of_digits,-1,-2):
     if len(j)==2:
         dig_1_sObject = slice(0,1)
         dig_2_sObject = slice(1,2)
-        lst.append(j[dig_1_sObject])
-        lst.append(j[dig_2_sObject])
+        lst.append(int(j[dig_1_sObject]))
+        lst.append(int(j[dig_2_sObject]))
     else:
-        lst.append(j)
+        lst.append(int(j))
 print (lst)
+
+sum_lst=0
+
+for i in lst:
+    sum_lst += i
+
+print (sum_lst)
+
+num_of_digits = num_of_digits+1
+
+lst_2=[]
+
+for i in range(num_of_digits,-1,-2):
+    j = int(credit[i])
+    lst_2.append(j)
+
+print(lst_2)
+
+
+
+
+
+
+

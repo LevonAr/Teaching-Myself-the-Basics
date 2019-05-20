abd35ddb9e27e94f27dc2d0a6fb4108e419475bb
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-20 10:57:37 -0700

    Update credit.py

--- Python/credit.py
+++ Python/credit.py
@@ -9,5 +9,10 @@ lst = []
 for i in range(num_of_digits,-1,-2):
     j = str(int(credit[i])*2)
     if len(j)==2:
-        asdasd
-    lst.append(j)
+        dig_1_sObject = slice(0,1)
+        dig_2_sObject = slice(1,2)
+        lst.append(j[dig_1_sObject])
+        lst.append(j[dig_2_sObject])
+    else:
+        lst.append(j)
+print (lst)

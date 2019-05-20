152bda3a14167c10dc65c7657d6030d56f2b9b04
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-20 14:32:55 -0700

    Update credit.py

--- Python/credit.py
+++ Python/credit.py
@@ -34,9 +34,32 @@ for i in range(num_of_digits,-1,-2):
 
 print(lst_2)
 
+sum_lst_2=0
 
+for i in lst_2:
+    sum_lst_2 += i
 
+print (sum_lst_2)
 
+check = sum_lst+sum_lst_2
 
+slice_first_2_digits = slice(0,2)
+slice_first_digit = slice(0,1)
 
-
+if check%10==0:
+    if pre_num_of_digits==15 & credit[slice_first_2_digits]=='34' or '37':
+        print("AMEX")
+    elif pre_num_of_digits==13 & credit[slice_first_digit]=='4':
+        print("VISA")
+    elif pre_num_of_digits==16:
+        if credit[slice_first_2_digits]=='51'or'52'or'53'or'54'or'55':
+            print("MASTERCARD")
+        elif credit[slice_first_digit]=='4':
+            print("VISA")
+        else:
+            print("INVALID")
+    else:
+        print("INVALID")
+        
+else:
+    print("INVALID")

dc7bdf44585aae92b344073af7225693f37ec058
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-20 23:00:30 -0700

    Update credit.py

--- Python/credit.py
+++ Python/credit.py
@@ -22,14 +22,11 @@ for i in range(num_of_digits,-1,-2):
         lst.append(int(j[dig_2_sObject]))
     else:
         lst.append(int(j))
-print (lst)
 
 sum_lst=0
 
 for i in lst:
     sum_lst += i
-
-print (sum_lst)
 
 num_of_digits = num_of_digits+1
 
@@ -39,27 +36,25 @@ for i in range(num_of_digits,-1,-2):
     j = int(credit[i])
     lst_2.append(j)
 
-print(lst_2)
-
 sum_lst_2=0
 
 for i in lst_2:
     sum_lst_2 += i
-
-print (sum_lst_2)
 
 check = sum_lst+sum_lst_2
 
 slice_first_2_digits = slice(0,2)
 slice_first_digit = slice(0,1)
 
+
+
 if check%10==0:
-    if pre_num_of_digits==15 and credit[slice_first_2_digits]=='34' or '37':
+    if (pre_num_of_digits==15) and (credit[slice_first_2_digits] in ('34','37')):
         print("AMEX")
-    elif pre_num_of_digits==13 and credit[slice_first_digit]=='4':
+    elif (pre_num_of_digits==13) and (credit[slice_first_digit]=='4'):
         print("VISA")
-    elif pre_num_of_digits==16:
-        if credit[slice_first_2_digits]=='51'or'52'or'53'or'54'or'55':
+    elif (pre_num_of_digits==16):
+        if credit[slice_first_2_digits] in ('51','52','53','54','55'):
             print("MASTERCARD")
         elif credit[slice_first_digit]=='4':
             print("VISA")

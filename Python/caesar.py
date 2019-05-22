34edf4eab7e12a9cb838a5a2feb9c066829ec29f
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-22 14:02:15 -0700

    Update caesar.py

--- Python/caesar.py
+++ Python/caesar.py
@@ -3,25 +3,18 @@ import sys
 if len(sys.argv)!=2:
     print("Usage: ./caesar k")
     sys.exit(1)
-        
+
 K = int(sys.argv[1])
 
 plaintext = input("plaintext:")
 
 polished_K = K%26
-        
-for i in plaintext:
-    ciphertext[i] = plaintext[i]
-    
-#proper form practice
-        
-def bad_append(new_item, a_list=[]):
-    a_list.append(new_item)
-    return a_list
-    
-def good_append(new_item, a_list=None):
-    if a_list is None:
-        a_list = []
-    a_list.append(new_item)
-    return a_list
 
+ciphertext = ""
+
+for i in plaintext:
+    j = chr(ord(i) + polished_K)
+
+    ciphertext = ciphertext + j
+
+print (ciphertext)

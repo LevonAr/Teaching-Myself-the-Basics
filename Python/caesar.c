718afdc5462dd82499a74cd80a5b09316bbfc20f
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-21 23:16:07 -0700

    Update caesar.c

--- Python/caesar.c
+++ Python/caesar.c
@@ -24,7 +24,11 @@ def good_append(new_item, a_list=None):
         a_list = []
     a_list.append(new_item)
     return a_list
+
 print (bad_append('one'))
 
 print (bad_append('two'))
 
+print (good_append('one'))
+
+print (good_append('two'))

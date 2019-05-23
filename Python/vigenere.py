4576a132be0e101729d92e80beabc0fbb52f61d2
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-22 23:26:51 -0700

    Update vigenere.py
    trying to work with itertools.cycle() and next()

--- Python/vigenere.py
+++ Python/vigenere.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 if len(sys.argv)!=2:
     print("Usage: python vigenere.py [k]")
@@ -18,4 +19,25 @@ for i in key:
     else:
         print("error: use only alphabetical letters as your key")
         sys.exit(1)
+new_toy = itertools.cycle(key_list)
 
+plaintext = input("plaintext:")
+
+ciphertext = ""
+
+for i in plaintext:
+    if i.isupper():
+        j = ord(i) + next(new_toy)
+        if j > 90:
+            j = j-26
+        j = chr(j)
+    elif i.islower():
+        j = ord(i) + next(new_toy)
+        if j > 122:
+            j = j -26
+        j = chr(j)
+    else:
+        j = i
+        next(new_toy)
+
+ciphertext = ciphertext + j

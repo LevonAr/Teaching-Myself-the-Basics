283d0e96fe9bead900b9dfdb4457d9c74f7edb7a
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-24 12:50:50 -0700

    Update crack.py
    I want to make it more effecient by checking all single letter pw first, my old program checked 'a' then 'aa' then 'aaa' an so on, which is not efficient because its quicker to check all shorter pw's first

--- Python/crack.py
+++ Python/crack.py
@@ -6,12 +6,38 @@ if len(sys.argv)!=2:
     print("usage: python crack.py [hash]")
     sys.exit(1)
 
-hash = sys.argv[1]
+_hash = sys.argv[1]
 
-salt = hash[0:2]
+salt = _hash[0:2]
 
-for i in itertools.chain(range(65,90), range(97,122)):
-    test_key = chr(i)
-    test_hash = crypt.crypt(test_key, salt)
-    if test_hash == hash:
+for i in itertools.chain(range(64,91), range(97,123)):
+    first_char = chr(i)
+    one_char_pw = first_char
+    print(one_char_pw)
+    one_char_hash = crypt.crypt(one_char_pw, salt)
+    if one_char_pw == "@":
         
+    
+    elif one_char_hash == _hash:
+        print(first_char)
+        sys.exit(0)
+    else:
+        for j in itertools.chain(range(64,91), range(97,123)):
+            second_char = chr(j)
+            two_char_pw = first_char + second_char
+            print(two_char_pw)
+            two_char_hash = crypt.crypt(one_char_pw, salt)
+            if
+
+
+
+
+
+
+
+
+
+    #test_key = chr(i)
+    #test_hash = crypt.crypt(test_key, salt)
+    #if test_hash == hash:
+

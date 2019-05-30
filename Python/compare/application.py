db67961e2a2ff141183d3e20b8885c1b2259f106
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-05-29 23:38:02 -0700

    Update application.py

--- Python/compare/application.py
+++ Python/compare/application.py
@@ -3,3 +3,7 @@ import re
 from flask import Flask, abort, redirect, render_template, request
 from html import escape
 from werkzeug.exceptions import default_exceptions, HTTPException
+from helpers import lines, sentences, substrings
+
+# Web app
+app = Flask(__name__)

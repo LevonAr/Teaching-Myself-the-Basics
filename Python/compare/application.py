e8787ce9d177f5dad18bce76bbce1fd600111b1a
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-06-02 13:04:52 -0700

    Update application.py

--- Python/compare/application.py
+++ Python/compare/application.py
@@ -72,3 +72,4 @@ def highlight(s, regexes):
         for match in matches:
             intervals.append((match.start(), match.end()))
     intervals.sort(key=lambda x: x[0])
+    # Combine intervals to get highlighted areas

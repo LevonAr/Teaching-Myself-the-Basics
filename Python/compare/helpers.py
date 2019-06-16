0ef724b9873a5da65bcfe41c488799d5561ead9c
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-06-15 20:15:09 -0700

    Update helpers.py

--- Python/compare/helpers.py
+++ Python/compare/helpers.py
@@ -4,7 +4,7 @@ def lines(a, b):
     lines_list_B = str.splitlines(b)
 
     if len(lines_list_A) > len(lines_list_B):
-        list_len = len(lines_list_B)
-                
+        list_len = len(lines_list_B)                
     elif len(lines_list_B) > len(lines_list_A):        
         list_len = len(lines_list_A)
+    elif len(lines_list_A) == len(lines_list_B):

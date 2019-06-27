cdf3b43960a381d610674399b4e8d0a06813d53e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-06-26 23:52:49 -0700

    Update application.py

--- Python/ChoreWheel/application.py
+++ Python/ChoreWheel/application.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def override_url_for():
     return dict(url_for=dated_url_for)
 
+def dated_url_for(endpoint, **values):
+
 @app.route("/")
 def index():
     return render_template("index.html")

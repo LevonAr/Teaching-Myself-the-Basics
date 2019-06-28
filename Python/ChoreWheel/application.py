480de034f9fb55a4e34f991d35fa0d1172ced9d1
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-06-28 07:42:08 -0700

    Update application.py

--- Python/ChoreWheel/application.py
+++ Python/ChoreWheel/application.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html", AJ = 'variable')
 
+def index():
+    return render_template("index.html", peeps = people_of_mansion, n=n)
 

0a60feb1a39f07f4bc3675c91debafae2505221d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-06-30 23:38:53 -0700

    Create snippets.py

---
+++ Python/ChoreWheel/snippets.py
@@ -0,0 +1,9 @@
+#def rev_str(my_str):
+ #   length = len(my_str)
+  #  for i in range(length - 1,-1,-1):
+   #     yield my_str[i]
+#
+#ls = [0,1,2,3,4,5,6,7]
+#
+#for char in rev_str(ls):
+#     print(char)

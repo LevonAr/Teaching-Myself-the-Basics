e683bfcb38ef5aad0bce4806f4aa331ab6a55d5c
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-01 23:47:39 -0700

    Update snippets.py

--- Python/ChoreWheel/snippets.py
+++ Python/ChoreWheel/snippets.py
@@ -19,3 +19,27 @@ print(next(s))
 print(next(s))
 print(next(s))
 print(next(s))
+
+
+key_list = []
+
+for i in key:
+    if i.isupper():
+        a = ord(i)-65
+        key_list.append(a)
+    elif i.islower():
+        a = ord(i)-97
+        key_list.append(a)
+    else:
+        print("error: use only alphabetical letters as your key")
+        sys.exit(1)
+
+
+new_toy = itertools.cycle(key_list)
+
+
+
+
+print (next(new_toy))
+print (next(new_toy))
+print (next(new_toy))

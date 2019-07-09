3a520c129c7bb1cb0a1094683a254836d6baa4f5
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-08 23:07:07 -0700

    Update application.py

--- Python/stack_practice/application.py
+++ Python/stack_practice/application.py
@@ -5,4 +5,11 @@ pp = Flask(__name__)
 
 @app.route("/")
 def index():
+    people_of_mansion = ["Ried", "Salvador", "Mo", "Madison", "Vaughn", "Mario", "Kat", "Mejan", "Liv", "Gabe", "Frosty","Drake"]
+
+    n=11
+
+    people_of_mansion_looped = cycle(people_of_mansion)
+
+    return render_template("index.html", peeps = people_of_mansion_looped, n=n)
     

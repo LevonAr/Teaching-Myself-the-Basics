ab1df81e6a3f57b90b0eefa953302051507cb40b
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-09 18:37:13 -0700

    Update application.py

--- Python/ChoreWheel/application.py
+++ Python/ChoreWheel/application.py
@@ -7,23 +7,34 @@ app = Flask(__name__)
 def index():
     people_of_mansion = ["Ried", "Salvador", "Mo", "Madison", "Vaughn", "Mario", "Kat", "Mejan", "Liv", "Gabe", "Frosty","Drake"]
 
-    n=11
+    n=2
 
     people_of_mansion_looped = cycle(people_of_mansion)
 
-    def skip_days(n):
-        for i in range(n):
+    def skip_days(days):
+        for i in range(days):
             next(people_of_mansion_looped)
 
+    skip_days(n)
+
     fridge = []
+    trash = []
     living_room = []
     bathroom = [] 
-    trash = []
 
-    for i in range(4)
+    for i in range(3):
         fridge.append(next(people_of_mansion_looped))
+
+    for i in range(3):
+        trash.append(next(people_of_mansion_looped))
+
+    for i in range(3):
+        living_room.append(next(people_of_mansion_looped))
+
+    for i in range(3):
+        bathroom.append(next(people_of_mansion_looped))
 
               
     
-    return render_template("index.html", fridge = fridge )
+    return render_template("index.html", fridge = fridge, trash = trash, living_room = living_room, bathroom = bathroom,)
 

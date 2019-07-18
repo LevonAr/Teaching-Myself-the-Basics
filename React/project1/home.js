43d714d7e0c06ed9961d804ac46dd7c9ef8019d1
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-17 23:46:31 -0700

    Update home.js

--- React/project1/home.js
+++ React/project1/home.js
@@ -49,3 +49,22 @@ export default function HomeScreen() {
           </TouchableOpacity>
         </View>
       </ScrollView>
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>
+          This is a tab bar. You can edit it in:
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+          <MonoText style={styles.codeHighlightText}>
+            navigation/MainTabNavigator.js
+          </MonoText>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};

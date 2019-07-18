0386643084a40350f686e64a361cf1a4e83a05bc
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-17 23:43:39 -0700

    Create home.js

---
+++ React/project1/home.js
@@ -0,0 +1,15 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {

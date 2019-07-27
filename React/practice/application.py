e3cbc14cff765c46f16b430188d44eecf9cc9100
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-07-26 23:43:03 -0700

    Update application.py

--- React/practice/application.py
+++ React/practice/application.py
@@ -4,3 +4,6 @@ import urllib.request
 
 from flask import Flask, jsonify, render_template, request
 from flask.exthook import ExtDeprecationWarning
+from warnings import simplefilter
+simplefilter("ignore", ExtDeprecationWarning)
+from flask_autoindex import AutoIndex

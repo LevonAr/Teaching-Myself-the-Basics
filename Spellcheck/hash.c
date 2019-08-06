8c96a500178c7d1ef8187898024402afea75a8ab
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-08-05 23:53:26 -0700

    Update hash.c

--- Spellcheck/hash.c
+++ Spellcheck/hash.c
@@ -1,1 +1,85 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include "dictionary.h"
+#include <stdlib.h>
+#include <string.h>
 
+
+typedef struct _node
+{
+    char* word;
+
+    struct _node *next;
+} node ;
+
+typedef struct
+{
+    int size;
+
+    node **words;
+} Hashtable;
+
+
+int dict_size;
+
+Hashtable* HT_Ptr;
+
+//just checking if my linked list works
+int linked_list_counter;
+
+int hash_table_size;
+
+
+// Forward Declarations:
+
+unsigned int PJWHash(const char* str, unsigned int length);
+
+char* trim(char word[], int len);
+
+int prime(int ht_size);
+
+node* makeLink(void);
+
+node* addLink(node* link, char* add_word);
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    //turn all words into lowercase because directions specify check to be case insensitive and all words in dictionary are required to be lowercase
+    char* lowercase_word = malloc((strlen(word)+1)*sizeof(char)) ;
+
+    int i;
+
+    for(i=0; i<strlen(word); i++)
+    {
+        lowercase_word[i] = tolower(word[i]);
+    }
+
+    lowercase_word[i]= '\0';
+
+    int pre_check_index = PJWHash(lowercase_word,strlen(word));
+
+    int check_index = pre_check_index % hash_table_size;
+
+    if(HT_Ptr->words[check_index])
+    {
+        if(strcmp(HT_Ptr->words[check_index]->word, lowercase_word)==0)
+        {
+            return true;
+        }
+
+        bool check_bucket = false;
+
+
+        for(node* Ptr = HT_Ptr->words[check_index]; Ptr->next != NULL; Ptr = Ptr->next)
+            {
+                if(strcmp(Ptr->next->word, lowercase_word)==0)
+                {
+                    check_bucket = true;
+                    int* LLP = &linked_list_counter;
+                    *LLP = linked_list_counter + 1;
+                    break;
+                }

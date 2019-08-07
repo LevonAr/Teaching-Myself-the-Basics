f29cd69870635cdec90f3123f2364593f7008ddc
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-08-06 23:49:41 -0700

    Create log4.c

---
+++ Spellcheck/attempts/log4.c
@@ -0,0 +1,151 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include "dictionary.h"
+#include <stdlib.h>
+#include <string.h>
+
+// technically alphabet and an apostrophe
+#define alphabet (27)
+
+typedef struct trie_
+{
+    struct trie_* nodes[alphabet];
+
+    bool end_of_word;
+} trie;
+
+trie* init_node(void)
+{
+    trie* temp = malloc(sizeof(trie));
+
+    if(!temp)
+    {
+        perror("error: not enough memory to initaize node");
+    }
+
+    temp->end_of_word = false;
+
+    int i;
+
+    for(i=0; i<alphabet; i++)
+    {
+        temp->nodes[i] = NULL;
+    }
+
+    return temp;
+}
+
+
+void add_entry(trie* claw, const char* input_word)
+{
+    // double for loop that increments chars in the word and increments through the trie pointers accordingly
+    int i;
+
+    //trie* claw = init_node();
+    
+    // run valgrind on control(trie.c) and experiment
+
+    int node_of_letter;
+
+    //claw = root;
+    
+    for(i=0; i<strlen(input_word); i++)
+    {
+        //convert letter to corresponsing trie node
+
+
+        node_of_letter = (int)input_word[i] - 97;
+
+        if(input_word[i]== '\'')
+        {
+            node_of_letter = 26;
+        }
+
+
+        if (claw->nodes[node_of_letter])
+        {
+            claw = claw->nodes[node_of_letter];
+        }
+
+        else
+        {
+            claw->nodes[node_of_letter] = init_node();
+
+            claw = claw->nodes[node_of_letter];
+        }
+    }
+
+    claw->end_of_word = true;
+}
+
+char* trim(char word[], int len)
+{
+    char* storage = malloc(sizeof(char)*(len+1));
+
+    for(int i=0; i<=len; i++)
+    {
+        storage[i] = word[i];
+
+    }
+
+    return storage;
+}
+
+trie base_root;
+
+int dict_size;
+
+bool check(const char *word)
+{
+    char* lowercase_word = malloc((strlen(word)+1)*sizeof(char)) ;
+
+    int i;
+
+    for(i=0; i<strlen(word); i++)
+    {
+        lowercase_word[i] = tolower(word[i]);
+    }
+
+    lowercase_word[i]= '\0';
+
+    trie* check_claw = init_node();
+
+    check_claw = &base_root;
+
+    int node_of_letter;
+
+    int j;
+
+    for(j=0; j<strlen(lowercase_word); j++)
+    {
+        node_of_letter = (int)lowercase_word[j] - 97;
+
+        if(lowercase_word[j]== '\'')
+        {
+            node_of_letter = 26;
+        }
+
+        if(check_claw->nodes[node_of_letter])
+        {
+            check_claw = check_claw->nodes[node_of_letter];
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    if(check_claw->end_of_word == true)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}

56506f92e3879554c6884e01e02ee53607bea034
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-08-30 23:12:39 -0700

    Update closures.js

--- React/practice/closures.js
+++ React/practice/closures.js
@@ -12,3 +12,6 @@ const sayHello = makeHelloFunction()
 
 // the variable called message is not in scope here
 console.log('typeof message:', typeof message)
+
+// but the function sayHello still references a variable called message
+console.log(sayHello.toString())

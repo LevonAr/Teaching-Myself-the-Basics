c073d03ec72e21dc8815f2bf577879619e7174b1
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-09-02 23:41:11 -0700

    Update immediately_invoked_function.js

--- React/practice/immediately_invoked_function.js
+++ React/practice/immediately_invoked_function.js
@@ -1,1 +1,4 @@
-
+// this creates the same closure as in 1-closureExample.js, but doesn't pollute
+// the global scope with a function called makeHelloFunction like that example
+const sayHello = (function () {
+  var message = 'Hello!'

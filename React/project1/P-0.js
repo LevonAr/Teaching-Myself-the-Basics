ea3d46b6a59c1c4035d1ddfdcf674679813ee1af
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-09 23:56:49 -0700

    Update P-0.js

--- React/project1/P-0.js
+++ React/project1/P-0.js
@@ -3,3 +3,5 @@ const list = document.getElementById('todo-list');
 const itemCountSpan = document.getElementById('item-count');
 
 const itemCountSpan = document.getElementById('item-count');
+
+function renderCounters

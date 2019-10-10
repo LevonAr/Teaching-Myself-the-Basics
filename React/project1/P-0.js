390e0cc29a65c00d9aeec196d4906626ed38ab14
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-10-09 23:59:28 -0700

    Update P-0.js

--- React/project1/P-0.js
+++ React/project1/P-0.js
@@ -11,3 +11,18 @@ function renderCounters() {
 
 function addTodo(todo) {
   itemCount += 1;
+
+let itemCount = 0;
+let uncheckedCount = 0;
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
+
+function renderCounters() {
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+  itemCountSpan.innerHTML = itemCount;
+}
+
+function addTodo(todo) {
+  itemCount += 1;
+
